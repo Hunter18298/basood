44c566f4dae40c953a648961a711ae9c0dd7aa88
Hunter18298 <aland.goran182@gmail.com>
2023-11-30 13:48:28 +0300

    done

--- app/introduction.tsx
+++ app/introduction.tsx
@@ -15,7 +15,7 @@ export default function Introduction() {
                     <strong className="text-yellow-500 stroke-red-900">B</strong>asood Company
                 </h1>
                 <p className="text-sm sm:text-lg text-justify leading-7 sm:leading-8">
-                    The heart of animal care since 2021. As Iraq's leading supplier of
+                    The heart of animal care since 2021. As Iraq`s leading supplier of
                     Basood products, we offer a wide range of animal medicines and
                     nutritious food. Experience compassionate veterinary care at our KADAN
                     VETERINARY CLINIC in Erbil, 60 Meter Street, or enjoy the convenience
